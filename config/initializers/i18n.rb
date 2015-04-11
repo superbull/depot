e2f4d765f073b2c695d2d0e5b6740f559b21dde0
Deng Zhi Cheng <superbulldeng@gmail.com>
2015-04-12 07:52:19 +0800

    i18n, except some translations

---
+++ config/initializers/i18n.rb
@@ -0,0 +1,6 @@
+I18n.default_locale = :en
+
+LANGUAGES = [
+  ['English', 'en'],
+  ['简体中文'.html_safe, 'zh-CN']
+]
--- config/routes.rb
+++ config/routes.rb
@@ -6,21 +6,29 @@ Rails.application.routes.draw do
     delete 'logout' => :destroy
   end
 
+  get "sessions/create"
+  get "sessions/destroy"
+
   resources :users
-  resources :orders
-  resources :line_items
-  resources :carts
-  get 'store/index'
 
   resources :products do
     get :who_bought, on: :member
   end
+  
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store'
+  end
+
+  # get 'store/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root 'store#index', as: 'store'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
